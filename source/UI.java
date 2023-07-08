b3e43b93453c10683c216571534d4e2c152344b0
Henry Kong <konghenry85@gmail.com>
2023-07-07 23:29:05 -0700

    added panel transitions when buttons are pressed

--- source/UI.java
+++ source/UI.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -10,6 +12,9 @@ public class UI{
 
     static Object [][] data;
     static final String [] columnNames = {"Site","Email","Username","password"};
+
+    JFrame mainFrame;
+    JPanel contentDisplay;
     public static void main(String [] args){       
         UI gui = new UI();
         data = getData();
@@ -18,7 +23,7 @@ public class UI{
 
     public void createUI(){
         //create the top level frame that hold everything
-        JFrame mainFrame = new JFrame();
+        mainFrame = new JFrame();
 
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(1000,600);
@@ -47,35 +52,37 @@ public class UI{
         //create a panel to hold the second row of buttons
         JPanel firstRow = new JPanel(new FlowLayout(FlowLayout.LEFT,0,0));
 
-        JButton newItem = new JButton("New");
-        JButton generatePassword = new JButton("Generate Password");
+        JButton newItemButton = new JButton("New");
+        JButton genPasswordButton = new JButton("Generate Password");
         JButton copy = new JButton("Copy to Clipboard");
-        firstRow.add(newItem);
-        firstRow.add(generatePassword);
+        firstRow.add(newItemButton);
+        firstRow.add(genPasswordButton);
         firstRow.add(copy);
 
+        //adding event listeners
+        genPasswordButton.addActionListener(new GenPasswordButtonListener());
+        newItemButton.addActionListener(new NewItemButtonListener());
 
         optionsMenu.add(firstRow);
         JSeparator divider = new JSeparator(SwingConstants.HORIZONTAL);
         optionsMenu.add(divider);
 
         //creating content display section
-        JPanel contentDisplay = new JPanel(new BorderLayout());
+        contentDisplay = new JPanel(new BorderLayout());
         contentDisplay.setBorder(new EmptyBorder(10,10,10,10));
 
         JTable dataTable = new JTable(data, columnNames);
         JScrollPane scrollPane = new JScrollPane(dataTable);
-        //JButton placeholder = new JButton("content display placeholder");
         contentDisplay.add(scrollPane);
 
         //putting the sections into root layout
         mainFrame.getContentPane().add(BorderLayout.NORTH, optionsMenu);
         mainFrame.getContentPane().add(BorderLayout.CENTER, contentDisplay); 
+        //mainFrame.pack();
 
         //making everything visible
         mainFrame.setVisible(true);
         optionsMenu.setVisible(true);
-       // firstRow.setVisible(true);
         firstRow.setVisible(true);
         contentDisplay.setVisible(true);
 
@@ -101,6 +108,41 @@ public class UI{
         } catch(Exception e){
             e.printStackTrace();
             return null;
+        }
+    }
+
+    class GenPasswordButtonListener implements ActionListener{
+        public void actionPerformed (ActionEvent event){
+            //mainFrame.getContentPane().remove(contentDisplay);
+            contentDisplay.removeAll();
+            createGeneratePasswordUI();
+            contentDisplay.revalidate();
+            contentDisplay.repaint();   
+        }
+        public void createGeneratePasswordUI(){
+            JPanel panel = new JPanel(new FlowLayout());
+            JTextField placeholder = new JTextField("placeholder");
+            JButton generate = new JButton("Generate");
+            panel.add(placeholder);
+            panel.add(generate);
+            contentDisplay.add(panel);
+        }
+    }
+
+    class NewItemButtonListener implements ActionListener{
+        public void actionPerformed(ActionEvent event){
+            contentDisplay.removeAll();
+            createDefaultUI();
+            contentDisplay.revalidate();
+            contentDisplay.repaint();  
+        }
+        
+        public void createDefaultUI(){
+            JPanel panel = new JPanel(new BorderLayout());
+            JTable dataTable = new JTable(data, columnNames);
+            JScrollPane scrollPane = new JScrollPane(dataTable);
+            panel.add(scrollPane);
+            contentDisplay.add(panel);
         }
     }
 }
