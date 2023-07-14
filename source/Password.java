1a9971c0988bf30296249f29ce08a8d71b161411
Henry Kong <konghenry85@gmail.com>
2023-07-14 00:14:47 -0700

    added method to test password strength

--- source/Password.java
+++ source/Password.java
@@ -10,7 +10,7 @@ public class Password {
         String password = "hockey";
         String password2 = "football";
         
-        int score = calculatePasswordStrength(password);
+        //int score = calculatePasswordStrength(password);
         String generatedPassword = generatePassword();
         System.out.println("The generated password is: "+generatedPassword);
         System.out.println("Searching Results Found = :"+dictionary.search(password));
@@ -18,6 +18,8 @@ public class Password {
         
         System.out.println("Password Generation");
         System.out.println(Password.generatePassword());
+        System.out.println("Checking password strength of:" +password);
+        System.out.println(checkPasswordStrength(password));
     }
     
     private static void loadDictionary() throws IOException {
@@ -51,5 +53,57 @@ public class Password {
 
     public static void printResults() {
 
+    }
+
+    public static String checkPasswordStrength(String password) {
+        int score = 0;
+        int length = password.length();
+        StringBuilder result = new StringBuilder("Password Strength: ");
+        //checking length
+        if(length >= 8 && length <=10) {
+            score += 1;
+        }
+        else if(length >=11 && length <=15) {
+            score += 2;
+        }
+        else if(length >=16) {
+            score += 3;
+        }
+
+        //check for characteristics
+        if(password.matches("(?=.*[a-z]).*")) {  //check for lowercase
+            score++;
+        }
+        if(password.matches("(?=.*[A-Z]).*")) {  //check for uppercase
+            score++;
+        }
+        if(password.matches("(?=.*[0-9]).*")) {  //check for numbers
+            score++;
+        }
+        if(password.matches("(?=.*[!@#$%^&*]).*")) {  //check for special characters
+            score++;
+        }
+
+        switch(score) {
+            case 0: 
+                result.append("N/A. You left the field blank.");
+                break;
+            case 1: 
+            case 2: 
+            case 3: 
+                result.append("Weak");
+                break;
+            case 4:
+            case 5:
+                result.append("Moderate");
+                break;
+            case 6:
+            case 7:
+                result.append("Strong");
+                break;
+            default: 
+                result.append("Error. Scoring out of specified ranges.");
+        }
+        return result.toString();
     }
 }
