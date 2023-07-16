1b6de50a5de658ebf52081558b2c6729f7cec076
Henry Kong <konghenry85@gmail.com>
2023-07-15 22:47:01 -0700

    connected password tester backend to UI

--- source/Listeners/PasswordGeneratorButtonListener.java
+++ source/Listeners/PasswordGeneratorButtonListener.java
@@ -1,12 +1,9 @@
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 class PasswordGeneratorButtonListener implements ActionListener {
@@ -33,13 +30,31 @@ class PasswordGeneratorButtonListener implements ActionListener {
                 placeholder.setText(pass);
             }
         });
-
+        //creating panels for password testing layout
+        JPanel resultLabelHolder = new JPanel();
+        JPanel textBoxAndButtonHolder = new JPanel();
         JPanel passwordTestPanel = new JPanel();
+        passwordTestPanel.setLayout(new BoxLayout(passwordTestPanel, BoxLayout.Y_AXIS));
+        //panel holds result output label
+        JLabel passwordStrengthResults = new JLabel("Password Strength: ?");
+        resultLabelHolder.add(passwordStrengthResults);
+        //panel holds textbox and button
         JTextField textField = new JTextField("Enter Password", 20);
         JButton testPasswordButton = new JButton("Test Password");
+        textBoxAndButtonHolder.add(textField);
+        textBoxAndButtonHolder.add(testPasswordButton);
 
-        passwordTestPanel.add(textField);
-        passwordTestPanel.add(testPasswordButton);
+        //listener: this will test password and output result
+        testPasswordButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event){
+                String pass = textField.getText();
+                String result = Password.checkPasswordStrength(pass);
+                passwordStrengthResults.setText(result);
+            }
+        });
+        //adding everything to a parent panel
+        passwordTestPanel.add(resultLabelHolder);
+        passwordTestPanel.add(textBoxAndButtonHolder);
         generatePassJPanel.add(placeholder);
         generatePassJPanel.add(generate);
         panel.add(generatePassJPanel);
