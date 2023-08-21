fc87329ec2a02f2b00f54fb0e792b45a86fcad81
Richard Varela <rvarela@csus.edu>
2023-08-21 16:14:45 -0700

    Add all necessary license info

--- source/View/Listeners/RecordsTableListener.java
+++ source/View/Listeners/RecordsTableListener.java
@@ -1,3 +1,19 @@
+/* This file is part of Kings Key project
+ * Copyright (C) 2023 Richard E. Varela (rvare) and Henry Kong.
+ * 
+ * Kings Key is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kings Key is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR PARTICULAR PURPOSE. See GNU General Public Licnese for more details.
+ * 
+ * You should have recieved a copy of the GNU General Public License along
+ * with Kings Key. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package View.Listeners;
 
 import Model.*;
